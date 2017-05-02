6e4cfda152e8c72e53b977c62cab875d72fa7eaa
songinhong <sih4144@naver.com>
2017-05-02 15:04:36 +0900

    second commit

--- src/com/example/stack/TestMain.java
+++ src/com/example/stack/TestMain.java
@@ -3,6 +3,6 @@ package com.example.stack;
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		System.out.println("Song In Hong!!");
 	}
 }
